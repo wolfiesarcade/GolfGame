01138364e39d225f3f47037fd5e9947571ea21d8
wolfiesarcade <itoliverpeter2@gmail.com>
2025-02-09 12:34:49 -0500

    fixed bug with moves remaining not being calculated properly. added left, right, up, down, movement. fixed bug with roll dice loop.

--- GolfGame.py
+++ GolfGame.py
@@ -80,43 +80,144 @@ def GameLogic(game_terrain, rows, cols):
   internal_ball_row = ball_row
   internal_ball_col = ball_col
   space_left_up = internal_ball_row
+  space_left_right = cols - internal_ball_col
   space_left_down = rows - ball_row
   need_to_roll_dice = True
-  if need_to_roll_dice:
-    roll_dice = random.randint(1, 6)  # we roll the dice
-    print(f'you rolled a {roll_dice}!!')
-  moves_left = roll_dice
 
   while not_in_hole:
-    movement_query = input("how do you want to move the ball? (up, down, left, right)")
+    if need_to_roll_dice:
+      roll_dice = random.randint(1, 6)  # we roll the dice
+      print(f'you rolled a {roll_dice}!!') #self explanmatory
+      moves_left = roll_dice #we start off moves left with the number we rolled.
+    else:
+      moves_left = moves_left #bruh all i had to do was tell it "hey man if theres no need to roll the dice just use moves left instead rather than roll dice data. stupid."
+    movement_query = input("how do you want to move the ball? (up, down, left, right)")#basic question
     if movement_query == "up":
-      movement_up = int(input("How many spaces do you want to move the ball up?"))
-      if movement_up > roll_dice:
+      movement_up = int(input("How many spaces do you want to move the ball up?"))#we save the amount to movement up
+      if movement_up > moves_left: #if the movement up is greater than the moves left we tell them they cant move that far and we move on to the next iteration of the loop
         print(f'you only have {roll_dice} moves so you cannot move that far')
-      elif movement_up > space_left_up:
+      elif movement_up > space_left_up:# if the movement up is greater than the space left up we tell them they cant move that far and we move on to the next iteration of the loop
         print(f'you went out of bounds. remember you only have {space_left_up} moves up')
-        game_terrain[internal_ball_row][internal_ball_col] = '⚪'
-      elif game_terrain[internal_ball_row][internal_ball_col] == game_terrain[hole_row][hole_col]:
+        game_terrain[internal_ball_row][internal_ball_col] = '⚪'#only doing this because sometimes we wont be where we started and i want to make sure the ball is where it need to be. probably not needed but eh
+      elif game_terrain[internal_ball_row][internal_ball_col] == game_terrain[hole_row][hole_col]: #win condition. it needs more but not right now
         print("ayyy you got it in. good job")
         not_in_hole = False
+        break
+      else:
+        if internal_ball_col + 1 >= cols: #if the next column relative to the balls location is more than the amount of columns we still have to replace where the ball was with something so i chose what is to the left of the ball.
+          game_terrain[internal_ball_row][internal_ball_col] = game_terrain[ball_row][ball_col - 1]
+        elif internal_ball_col - 1 <= 0:  #if the next column relative to the balls location is more than the amount of columns we still have to replace where the ball was with something so i chose what is to the right of the ball.
+          game_terrain[internal_ball_row][internal_ball_col] = game_terrain[ball_row][ball_col + 1]
+        else:
+          game_terrain[internal_ball_row][internal_ball_col] = game_terrain[ball_row][ball_col + 1] #we replace the current ball location with a whatever is to its right.
+          internal_ball_row = internal_ball_row - movement_up #we change the internal ball row to the current location minus the movement up.
+          space_left_up = internal_ball_row #we change the space left up to the current location minus the movement up.
+          game_terrain[internal_ball_row][internal_ball_col] = '⚪' #we change the new current location to a ball.
+          print(f'you moved up {movement_up} spaces') #we tell them how many spaces they moved.
+          moves_left = moves_left - movement_up #we subtract the movement up from moves left.
+          print(f'you have {moves_left} moves left') # the math checks out so idont know why the code is being dumb
+          if moves_left == 0: #if there are no moves left then we need to roll the dice again.
+            need_to_roll_dice = True
+          else:#otherwise dont roll the dice again.
+            need_to_roll_dice = False
+          for row in game_terrain:
+            print(' '.join(row))
+    elif movement_query == "right":
+      movement_right = int(input("How many spaces do you want to move the ball right?"))#we save the amount to movement up
+      if movement_right > moves_left: #if the movement up is greater than the moves left we tell them they cant move that far and we move on to the next iteration of the loop
+        print(f'you only have {roll_dice} moves so you cannot move that far')
+      elif movement_right > space_left_right:# if the movement up is greater than the space left up we tell them they cant move that far and we move on to the next iteration of the loop
+        print(f'you went out of bounds. remember you only have {space_left_right} moves up')
+        game_terrain[internal_ball_row][internal_ball_col] = '⚪'#only doing this because sometimes we wont be where we started and i want to make sure the ball is where it need to be. probably not needed but eh
+      elif game_terrain[internal_ball_row][internal_ball_col] == game_terrain[hole_row][hole_col]: #win condition. it needs more but not right now
+        print("ayyy you got it in. good job")
+        not_in_hole = False
+        break
+      else:
+        if internal_ball_col + 1 >= cols: #if the next column relative to the balls location is more than the amount of columns we still have to replace where the ball was with something so i chose what is to the left of the ball.
+          game_terrain[internal_ball_row][internal_ball_col] = game_terrain[ball_row][ball_col - 1]
+        elif internal_ball_col - 1 <= 0:  #if the next column relative to the balls location is more than the amount of columns we still have to replace where the ball was with something so i chose what is to the right of the ball.
+          game_terrain[internal_ball_row][internal_ball_col] = game_terrain[ball_row][ball_col + 1]
+        else:
+          game_terrain[internal_ball_row][internal_ball_col] = game_terrain[ball_row][ball_col + 1] #we replace the current ball location with a whatever is to its right.
+          internal_ball_col = internal_ball_col - movement_right #we change the internal ball row to the current location minus the movement up.
+          space_left_right = internal_ball_col #we change the space left up to the current location minus the movement up.
+          game_terrain[internal_ball_row][internal_ball_col] = '⚪' #we change the new current location to a ball.
+          print(f'you moved right {movement_right} space/s') #we tell them how many spaces they moved.
+          moves_left = moves_left - movement_right #we subtract the movement up from moves left.
+          print(f'you have {moves_left} moves left') # the math checks out so idont know why the code is being dumb
+          if moves_left == 0: #if there are no moves left then we need to roll the dice again.
+            need_to_roll_dice = True
+          else:#otherwise dont roll the dice again.
+            need_to_roll_dice = False
+          for row in game_terrain:
+            print(' '.join(row))
+    elif movement_query == "down":
+      movement_down = int(input("How many spaces do you want to move the ball down?"))
+      if movement_down > moves_left: #if the movement up is greater than the moves left we tell them they cant move that far and we move on to the next iteration of the loop
+        print(f'you only have {roll_dice} moves so you cannot move that far')
+      elif movement_down > space_left_down:# if the movement up is greater than the space left up we tell them they cant move that far and we move on to the next iteration of the loop
+        print(f'you went out of bounds. remember you only have {space_left_down} moves up')
+        game_terrain[internal_ball_row][internal_ball_col] = '⚪'#only doing this because sometimes we wont be where we started and i want to make sure the ball is where it need to be. probably not needed but eh
+      elif game_terrain[internal_ball_row][internal_ball_col] == game_terrain[hole_row][hole_col]: #win condition. it needs more but not right now
+        print("ayyy you got it in. good job")
+        not_in_hole = False
+        break
       else:
         if internal_ball_col + 1 >= cols:
           game_terrain[internal_ball_row][internal_ball_col] = game_terrain[ball_row][ball_col - 1]
         elif internal_ball_col - 1 <= 0:
           game_terrain[internal_ball_row][internal_ball_col] = game_terrain[ball_row][ball_col + 1]
         else:
-          game_terrain[internal_ball_row][internal_ball_col] = '.'
-          internal_ball_row = internal_ball_row - movement_up
-          space_left_up = internal_ball_row
+          game_terrain[internal_ball_row][internal_ball_col] = game_terrain[ball_row][ball_col + 1]
+          internal_ball_row = internal_ball_row + movement_down
+          space_left_down = internal_ball_row
           game_terrain[internal_ball_row][internal_ball_col] = '⚪'
-          moves_left = moves_left - movement_up
+          print(f'you moved down {movement_down} spaces')
+          moves_left = moves_left - movement_down
           print(f'you have {moves_left} moves left')
-          for row in game_terrain:
-            print(' '.join(row))
           if moves_left == 0:
             need_to_roll_dice = True
           else:
             need_to_roll_dice = False
+          for row in game_terrain:
+            print(' '.join(row))
+    elif movement_query == "left":
+      movement_left = int(input("How many spaces do you want to move the ball left?"))
+      if movement_left > moves_left:
+        print(f'you only have {roll_dice} moves so you cannot move that far')
+      elif movement_left > space_left_right:
+        print(f'you went out of bounds. remember you only have {space_left_right} moves up')
+        game_terrain[internal_ball_row][internal_ball_col] = '⚪'#only doing this because sometimes we wont be where we started and i want to make sure the ball is where it need to be. probably not needed but eh
+      elif game_terrain[internal_ball_row][internal_ball_col] == game_terrain[hole_row][hole_col]: #win condition. it needs more but not right now
+        print("ayyy you got it in. good job")
+        not_in_hole = False
+        break
+      else:
+        if internal_ball_col + 1 >= cols:
+          game_terrain[internal_ball_row][internal_ball_col] = game_terrain[ball_row][ball_col - 1]
+        elif internal_ball_col - 1 <= 0:
+          game_terrain[internal_ball_row][internal_ball_col] = game_terrain[ball_row][ball_col + 1]
+        else:
+          game_terrain[internal_ball_row][internal_ball_col] = game_terrain[ball_row][ball_col + 1]
+          internal_ball_col = internal_ball_col - movement_left
+          space_left_right = internal_ball_col
+          game_terrain[internal_ball_row][internal_ball_col] = '⚪'
+          print(f'you moved left {movement_left} spaces')
+          moves_left = moves_left - movement_left
+          print(f'you have {moves_left} moves left')
+          if moves_left == 0:
+            need_to_roll_dice = True
+          else:
+            need_to_roll_dice = False
+          for row in game_terrain:
+            print(' '.join(row))
+    else:
+      print("invalid input. please try again.")
+      continue
+  return game_terrain
+
+
 
               
 
