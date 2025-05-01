8d9229d324a77c81b703518d7ed904f748b5cd96
wolfiesarcade <itoliverpeter2@gmail.com>
2025-04-30 20:20:38 -0400

    local multiplayer + penalty message added

--- golfrefined.py
+++ golfrefined.py
@@ -48,52 +48,37 @@ class GolfBall:
      self.col = random.randint(0, 9)
 class Multiplayer():
   def __init__(self):
-    P1_direction = " "
-    P2_direction = " "
-    P1_count = 0
-    P2_count = 1
     self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.clientsocket.connect(('localhost', 6969))
-
-  
-  #this is the request handler
-  # def Distribute(self,terrain):
-  #   self.clientsocket.send(terrain)
+    self.clientsocket.connect(('localhost', 6969))  # Only connect once here
 
   def DistributeTerrain(self, terrain):
     data = json.dumps(terrain)
     encoded = data.encode("utf-8")
     self.clientsocket.send(encoded)
     
-  def DistributeDice(self,diceresult):
-    dicedata = diceresult
-    diceroll = dicedata.to_bytes()
-    self.clientsocket.send(diceroll)
-  
-  def Receive_direction(self) -> str:
-    self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.serversocket.bind(('localhost', 6970))
-    self.serversocket.listen(2)
-    self.connection = self.serversocket.accept()[0]
-    buf = self.connection.recv(9999)
-    self.direction = buf.decode("utf-8")
-    print(self.direction)
-  def Recieve(self):
-    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serversocket.bind(('localhost', 6969))
-    serversocket.listen(2)
-    while True:
-      connection, address = serversocket.accept()
-      buf = connection.recv(10)
-      if self.P1_count < self.P2_count:
-        print(buf)
-      break
-  def NetworkTerrainRefiner(self, terrain) -> list:
-    b = bytearray()
-    data = json.dumps(terrain)
-    # for row in terrain:
-    #   self.Distribute(' '.join(row).encode())
+  def DistributeDice(self, diceresult):
+    diceroll = diceresult.to_bytes(1, byteorder='big')
+    self.clientsocket.sendall(diceroll)
 
+  def DistributeMovementcost(self, movementcost, playernumber):
+    if playernumber % 2 == 0:
+        clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientsocket.connect(('localhost', 6971))
+        clientsocket.send(movementcost.encode("utf-8"))
+            
+            
+  def Receive_direction(self, playernumber) -> str:
+    if playernumber % 2 == 0:
+      self.serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.serversocket.bind(('localhost', 6970))
+      self.serversocket.listen(1)
+      conn, _ = self.serversocket.accept()
+      buf = conn.recv(1024)
+      direction = buf.decode("utf-8")
+      print(direction)
+      conn.close()
+      self.serversocket.close()
+      return direction
     
 class GameLogic:
   def __init__(self, GolfBall, GolfCourse):
@@ -104,6 +89,7 @@ class GameLogic:
       game_row: int = 20
       game_col: int = 40
       self.parcount = 0
+      self.player = 0
       self.course = GolfCourse(game_row, game_col)#changed the order of things. first, make the course, 
       self.course.GenerateGolfCourse() #generate terrain, 
       self.ball = GolfBall(game_row) #make ball, 
@@ -111,8 +97,6 @@ class GameLogic:
       self.hole_row, self.hole_col = self.course.PlaceHole()#place hole, 
       self.multiplayer = Multiplayer()
       self.TerrainRefiner() #print refined terrain using joins. should prevent data from being overwritten this way.
-      self.clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      self.clientsocket.connect(('localhost', 6969))
 
   def Dice(self) -> int:
      self.dice_result =  random.randint(1,6)
@@ -123,23 +107,30 @@ class GameLogic:
     self.parcount+=1
   
   def MovementCost(self):
+    self.movementcostmessage = ' '
     if self.course.GetOriginalTerrain(self.ball.row,self.ball.col) == '#':
       sand_math = self.dice_result // 2
-      print(sand_math)
       self.dice_result = int(sand_math)
-      print('your movement is divided by 2 because you are in sand')
+      self.movementcostmessage='your movement is divided by 2 because you are in sand'
+      print(self.movementcostmessage)
     elif self.course.GetOriginalTerrain(self.ball.row,self.ball.col) == '~':
       water_math = self.dice_result // 3
       self.dice_result = int(water_math)
-      print('your movement is divided by 3 because you are in water')
+      self.movementcostmessage='your movement is divided by 3 because you are in water'
+      print(self.movementcostmessage)
     else:
       self.dice_result //= 1
       # print(math)
       # self.dice_result = math(int)
-      
 
-  def Movement(self) -> str:
-    direction = input("how do you want to move the ball? (up, down, left, right)")
+
+  def Movement(self, p2direction) -> str:
+    if self.player % 2 == 0:
+      print("how do you want to move the ball? (up, down, left, right)")
+      print(p2direction)
+      direction = p2direction
+    else:
+      direction = input("how do you want to move the ball? (up, down, left, right)")
     self.course.ReplaceTerrain(self.ball.row, self.ball.col) #setter method is easier to read
     print(f'you moved {direction} {self.dice_result} spaces') 
     if direction == "up": 
@@ -189,13 +180,14 @@ class GameLogic:
   def GameLoop(self) -> list:
     #since all that stuff was done before the loop, placed it in init instead, makes more sense
     while self.WinCondition():
+      self.player+=1
       self.multiplayer.DistributeTerrain(self.course.terrain)
       self.Dice() #random movement modifier
       self.multiplayer.DistributeDice(self.dice_result)
       self.ParCount()# par counter
       self.MovementCost()# duh
-      self.multiplayer.Receive_direction()
-      self.Movement() #movement decided based on direction input
+      self.multiplayer.DistributeMovementcost(self.movementcostmessage, self.player)
+      self.Movement(self.multiplayer.Receive_direction(self.player)) #movement decided based on direction input
       self.TerrainRefiner() #print the resulting change to the terrain and ball location
       # self.multiplayer.Distribute(self.course.terrain) #opushes each row to the client
 
